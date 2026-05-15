4dbe83c8ecb1447890e11e0b94807550f5bdb1d1
Rafael Barbosa <rafaelbarbosa102006@gmail.com>
2026-05-15 11:19:27 -0300

    Atualização da pagina do site

--- app/views.py
+++ app/views.py
@@ -4,9 +4,44 @@ from .models import Usuario, Aluno, Professor, Coordenador, Empresa, Vaga, Estag
 from .serializers import UsuarioSerializer, AlunoSerializer, ProfessorSerializer, CoordenadorSerializer, EmpresaSerializer, VagaSerializer, EstagioSerializer, DocumentoSerializer, RelatorioSerializer
 
 
-def home(request):
-    return HttpResponse("API do Sistema de Gestão de Estágios")
+from django.http import HttpResponse
 
+
+def home(request):
+    return HttpResponse("""
+        <h1>Sistema de Gestão de Estágios</h1>
+
+        <p>
+            API REST desenvolvida para gerenciamento de estágios,
+            permitindo controle de usuários, vagas, documentos
+            e acompanhamento institucional.
+        </p>
+
+        <h2>Funcionalidades</h2>
+
+        <ul>
+            <li>Cadastro de alunos</li>
+            <li>Cadastro de empresas</li>
+            <li>Gerenciamento de vagas</li>
+            <li>Controle de estágios</li>
+            <li>Envio de documentos</li>
+            <li>Geração de relatórios</li>
+        </ul>
+
+        <h2>Acessos</h2>
+
+        <ul>
+            <li><a href="/api/">API REST</a></li>
+            <li><a href="/admin/">Painel Administrativo</a></li>
+            <li><a href="/api/usuarios/">Usuários</a></li>
+            <li><a href="/api/alunos/">Alunos</a></li>
+            <li><a href="/api/empresas/">Empresas</a></li>
+            <li><a href="/api/vagas/">Vagas</a></li>
+            <li><a href="/api/estagios/">Estágios</a></li>
+            <li><a href="/api/documentos/">Documentos</a></li>
+            <li><a href="/api/relatorios/">Relatórios</a></li>
+        </ul>
+    """)
 
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
