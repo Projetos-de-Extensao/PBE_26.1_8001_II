ddc569afe8efb360f756c2bc4a6c8b6a6ce6c29a
DAVI COUTINHO <cdavi8249@gmail.com>
2026-06-03 09:57:57 -0300

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
 
@@ -21,4 +22,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('empresa/dashboard/', views.empresa_dashboard, name='empresa_dashboard'),
     path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
